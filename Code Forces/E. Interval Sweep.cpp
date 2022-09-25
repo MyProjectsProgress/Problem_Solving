bd105d7f08ec9750b3c1eb6b2d71a1224d5d6374
MagdyNasr41 <90320433+MagdyNasr41@users.noreply.github.com>
2022-09-25 19:58:23 +0200

    3 Problems
    Katroyshka needs another solution to pass time limit problem test number 104

---
+++ Code Forces/E. Interval Sweep.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    if ((a != 0) & (b != 0)){
+        if((a-b) == 1 || (b-a) == 1 || (a-b) == 0) cout<<"YES";
+        else cout<<"NO";
+    }
+    else cout<<"NO";
+    return 0;
+}
---
+++ Code Forces/G. Katryoshka.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // 458 81 280
+    bool flag1 = true, flag2 = true;
+    long long eye,mouth,body;
+    long long sum = 0;
+    cin>>eye>>mouth>>body;
+    if(eye == 0 || body == 0) {
+        cout<<0;
+        return 0;
+    }
+    else {
+        while(flag1){
+            if((mouth != 0) & (eye != 0) & (body != 0)){
+                sum++;
+                eye--;
+                mouth--;
+                body--;
+            }
+            else flag1 = false;
+        }
+        if((eye == 0) || (body == 0)) {
+        cout<<sum;
+        return 0;
+        }
+        else {
+            while(flag2){
+                if((eye <= 1) || (body <= 0)){
+                    flag2 = false;
+                }
+                else{
+                    sum++;
+                    eye = eye - 2;
+                    body--;
+                }
+            }
+            cout<<sum;
+            return 0;
+        }
+    }
+    return 0;
+}
