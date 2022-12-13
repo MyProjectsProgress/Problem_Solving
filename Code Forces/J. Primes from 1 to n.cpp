5ad3253f2c46f78c52d30441fde66f266faa4e5a
MagdyNasr41 <90320433+MagdyNasr41@users.noreply.github.com>
2022-12-14 00:39:12 +0200

    prime number problem and divisor problem

---
+++ Code Forces/J. Primes from 1 to n.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    bool flag = false;
+
+    cout<<2<<" ";
+    for(int i=2; i<=n; i++){
+        for(int j=2; j<i; j++){
+            if (i%j==0){
+                flag = false;
+                break;
+            }
+            else flag = true;
+        }
+        if (flag) cout<<i<<" ";
+    }
+}
