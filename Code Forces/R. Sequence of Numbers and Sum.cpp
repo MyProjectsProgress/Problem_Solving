a141fcad2f648547429c3203456fb273eae3f108
MagdyNasr41 <90320433+MagdyNasr41@users.noreply.github.com>
2022-12-18 16:04:51 +0200

    1 problem

--- Code Forces/R. Sequence of Numbers and Sum.cpp
+++ Code Forces/R. Sequence of Numbers and Sum.cpp
@@ -4,26 +4,28 @@ using namespace std;
 
 int main (){
     int x,y,sum{0},i{0};
+    bool flag = true;
     vector<int> minarr;
     vector<int> maxarr;
 
-    while(true){
-    cin>>x>>y;
-    if (x<=0 || y<=0) break;
-    minarr.push_back(min(x,y));
-    maxarr.push_back(max(x,y));
+    while(flag == true){
+        cin>>x>>y;
+        if (x<=0 || y<=0) flag = false;
+        else{
+        minarr.push_back(min(x,y));
+        maxarr.push_back(max(x,y));
+        }
     }
 
-    for(int i = 0; i<sizeof(minarr); i++){
-        for(int j = minarr[i]; j<maxarr[i]; j++){
-        sum += i;
-        cout<<i<<" ";
+    for(int i = 0; i<minarr.size(); i++){
+        for(int j = minarr[i]; j<=maxarr[i]; j++){
+        sum += j;
+        cout<<j<<" ";
         }
-        cout<<"sum = "<<sum<<endl;
+        cout<<"sum ="<<sum;
+        if(i<minarr.size()-1) cout<<endl;
         sum = 0;
     }
 
     return 0;
 }
-
-//for(int i = 0; i<sizeof(minarr); i++) cout<<"min= "<<minarr[i]<<"\t"<<"max= "<<maxarr[i]<<endl;
