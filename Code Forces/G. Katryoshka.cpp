18b8d44e930e3036353c57ccd771a90233e14a7a
MagdyNasr41 <90320433+MagdyNasr41@users.noreply.github.com>
2022-12-19 05:50:41 +0200

    2 problems 3al ree2 kda

--- Code Forces/G. Katryoshka.cpp
+++ Code Forces/G. Katryoshka.cpp
@@ -3,15 +3,35 @@
 using namespace std;
 
 int main(){
+    long long eye,mouth,body,res{0};
+    long long counter {0};
+    cin>>eye>>mouth>>body;
+
+    if(eye == 0 || body == 0) cout<<0;
+
+    else{
+        if((mouth >= eye && mouth >= body) || (mouth >= body && mouth < eye) || (mouth < body && mouth >= eye)) { res = min(eye,body); cout<<res; }
+
+        else if(mouth < body && mouth < eye)
+        {
+            res = mouth;
+            eye -= mouth;
+            body -= mouth;
+            res += min(eye/2, body);
+            cout<<res;
+        }
+    }
+
+    return 0;
+}
+//------------------------------------------------------------------
+/*
     bool flag1 = true, flag2 = true;
     long long eye,mouth,body;
     long long sum = 0;
     cin>>eye>>mouth>>body;
-
-
-//------------------------------------------------------------------
     // Time limit exceed code.
-    /* if(eye == 0 || body == 0) {
+    if(eye == 0 || body == 0) {
         cout<<0;
         return 0;
     }
@@ -43,5 +63,6 @@ int main(){
             cout<<sum;
             return 0;
         }
-    } */
+    }
 }
+*/
--- Code Forces/X. Two intervals.cpp
+++ Code Forces/X. Two intervals.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 
 int main(){
-    int x1,y1,x2,y2;
-    cin>>x1>>y1>>x2>>y2;
-    if     ((x2 > y1) & (y2 > y1)) cout<<-1;
-    else if((y2 < x1) & (x2 < x1)) cout<<-1;
-    else if((y1 > x2) & (y2 > y1)) cout<<x2<<" "<<y1;
-    else if((y1 > x2) & (y2 < y1)) cout<<x2<<" "<<y2;
-    else if((x1 < y2) & (y2 < y1)) cout<<x1<<" "<<y2;
-    else if((x1 < y2) & (y2 > y1)) cout<<x1<<" "<<y1;
-    else if((x1 < y2) & (y2 > y1)) cout<<x1<<" "<<y1;
-    else if((x2 < y1) & (y1 == y2))cout<<x2<<" "<<y2;
-    else if(x1 == y2 ) cout<<x1<<" "<<y2;
-    else if(x2 == y1 ) cout<<x2<<" "<<y1;
+    int num1,num2,num3,num4,start,endd;
+    cin>>num1>>num2>>num3>>num4;
+    if  ((num3 < num1) && (num4 < num1) || (num3 > num2) & (num4 > num2)) cout<<-1;
+    else{
+        if(num1 > num3) start = num1;
+        else   start = num3;
+
+        if(num2 > num4) endd = num4;
+        else endd = num2;
+
+        cout<<start<<" "<<endd;
+    }
 
     return 0;
 }
