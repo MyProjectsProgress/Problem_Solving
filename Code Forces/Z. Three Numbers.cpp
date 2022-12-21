c83738f61fc38125f09b852f33004ec28bf6ad3e
MagdyNasr41 <90320433+MagdyNasr41@users.noreply.github.com>
2022-12-21 11:40:49 +0200

    2 algorithm heap problems and 1 basic problem

--- Code Forces/Z. Three Numbers.cpp
+++ Code Forces/Z. Three Numbers.cpp
@@ -2,9 +2,24 @@
 
 using namespace std;
 
-}
-
 int main (){
+    int start,endd,counter{0};
+    cin>>start>>endd;
 
+    for(int i = 0; i<=start; i++)
+    for(int a = 0; a<=start; a++)
+    if( endd-i-a >= 0 && endd-i-a <= start ) counter++;
+    cout<<counter;
 }
+    /*
+    Time Limit Exceeded
+    for(int a = 0; a<=k; a++)
+    for(int b = 0; b<=k; b++)
+    for(int c = 0; c<=k; c++)
+    {
+        if(a+b+c==s) counter++;
+    }
+    cout<<counter;
+    */
+
 
