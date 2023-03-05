7f0e5825e3abd3749b8730fda7b3a1d0f105afef
MagdyNasr41 <90320433+MagdyNasr41@users.noreply.github.com>
2023-03-05 10:36:58 +0200

    1 problem

--- Code Forces/V. Frequency Array.cpp
+++ Code Forces/V. Frequency Array.cpp
@@ -7,10 +7,12 @@ int main(){
     int n,m,counter{0};
     cin>>n>>m;
     int arr[n];
-    vector<int> freq;
+    int freq[100001] = {0};
+    // vector<int> freq;
 
-    for(int i=0; i<n; i++) { int x; cin>>x; arr[i] = x; }
+    for(int i=0; i<n; i++) { int x; cin>>x; arr[i] = x; freq[x-1]++; }
 
+    /*
     for(int j=1; j<=m; j++)
     {
         for(int i=0; i<n; i++)
@@ -20,6 +22,7 @@ int main(){
         freq.push_back(counter);
         counter = 0;
     }
+    */
 
     for(int i=0; i<m; i++) { cout<<freq[i]<<endl; }
 
---
+++ Code Forces/W. Mirror Array.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int n,m;
+    cin>>n>>m;
+    int arr[n][m];
+
+    for(int i=0; i<n; i++)
+    for(int j=0; j<m; j++)
+    { int x; cin>>x; arr[i][m] = x; }
+
+    for(int j=0; j<n; j++)
+    {
+        for(int i=m-1; i>=0; i--)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
