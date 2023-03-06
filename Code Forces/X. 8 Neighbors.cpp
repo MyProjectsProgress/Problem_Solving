1acc808d7b1447224f26fd807d913b2cfe1a4257
MagdyNasr41 <90320433+MagdyNasr41@users.noreply.github.com>
2023-03-06 08:40:33 +0200

    2 problems but one is failed

--- Code Forces/X. 8 Neighbors.cpp
+++ Code Forces/X. 8 Neighbors.cpp
@@ -8,15 +8,17 @@ int main(){
 
     int n,m,x,y;
     cin>>n>>m;
-    char arr[n][m];
+    char arr[102][102] {'s'};
 
-    for(int i=0; i<n; i++)
-    for(int j=0; j<m; j++)
+    for(int i=1; i<=n; i++)
+    for(int j=1; j<=m; j++)
     { char x; cin>>x; arr[i][j] = x; }
 
-    cin>>x,y;
+    cin>>x>>y;
 
-
+    if(arr[x-1][y-1] != '.'&& arr[x][y-1] != '.' && arr[x+1][y-1] != '.' && arr[x-1][y] !='.' && arr[x+1][y]!='.'&&arr[x-1][y+1]!='.'&& arr[x][y+1] != '.'&& arr[x+1][y+1] != '.')
+    cout<<"yes";
+    else cout<<"no";
 
     return 0;
 }
---
+++ Code Forces/Y. Range sum query.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int n,q;
+    cin>>n>>q;
+    int arr[n];
+    long long sum {0};
+    vector<long long> sums;
+
+    for(int i=0; i<n; i++) { int x; cin>>x; arr[i] = x; }
+
+    while(q--){
+        int l,r;
+        cin>>l>>r;
+        for(int i=l-1; i<=r-1; i++) { sum+=arr[i]; }
+        sums.push_back(sum);
+        sum = 0;
+    }
+
+
+    for(int i=0; i<sums.size(); i++) { cout<<sums[i]<<endl; }
+
+    return 0;
+}
